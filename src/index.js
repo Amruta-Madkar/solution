890ea7ebf62c402325614f0fd3f5b5ef4e585e22
Amruta-Madkar <54406187+Amruta-Madkar@users.noreply.github.com>
2020-12-19 14:43:31 +0530

    First commit Message

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RegisterForm from './Component/RegisterFrom';
+
+
+ReactDOM.render(<RegisterForm /> , document.getElementById('root'));
+
